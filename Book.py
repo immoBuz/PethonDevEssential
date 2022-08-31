42c394d9d172e21416c6277468f9252cddb0891c
immo <busic@ukr.net>
2022-09-01 01:17:19 +0300

    Have created a project for HomeWork day 1

--- Book.py
+++ Book.py
@@ -5,3 +5,25 @@ class Book:
         self.__issue = issue_date
         self.__genre = genre
 
+    def get_author(self):
+        return self.__author
+
+    def get_name(self):
+        return self.__name
+
+    def get_issue_date(self):
+        return self.__issue
+
+    def get_genre(self):
+        return self.__genre
+
+    def get_info(self):
+        return self.__author, self.__name, self.__issue, self.__genre
+
+    def __eq__(self, other):
+        return (
+                self.get_author() == other.get_author()
+                and self.get_name() == other.get_name()
+                and self.get_issue_date() == other.get_issue_date()
+                and self.get_genre() == other.get_genre()
+        )
--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+from Book import Book
 
+
+book1 = Book(author='Marcus Perrie', name='Fighting back', issue_date='2019-01-01',genre='SciFi')
+print(book1.get_info())
+book2 = Book(author='Miranda Devis', name='Mystery sight', issue_date='2019-02-01', genre='Novel')
+print(book2.get_info())
+book3 = Book(author='Marcus Perrie', name='Fighting back', issue_date='2019-01-01',genre='SciFi')
+print(book3.get_info())
+print(book1 == book2)
+print(book1 == book3)
