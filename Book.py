bd658be011687d8d4a8e5b2fb5bf207c2d349e46
immo <busic@ukr.net>
2022-08-31 22:07:42 +0300

    Have created a project for HomeWork day 1

---
+++ Book.py
@@ -0,0 +1,7 @@
+class Book:
+    def __init__(self, author, name, issue_date, genre):
+        self.__author = author
+        self.__name = name
+        self.__issue = issue_date
+        self.__genre = genre
+
